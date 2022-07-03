3ec668ef7cd02fd5de43cb13b34132b585ec2176
prince482 <tejindersingh2202@gmail.com>
2022-07-03 20:53:48 +0530

    swapping of numbers

---
+++ swapping_number.py
@@ -0,0 +1,9 @@
+'''
+Swapping of numbers
+'''
+NUMBER_1 = int(input("First Number: "))
+NUMBER_2 = int(input("Second Number: "))
+NUMBER_1, NUMBER_2= NUMBER_2, NUMBER_1
+
+print("First Number:", NUMBER_1)
+print("Second Number:", NUMBER_2)

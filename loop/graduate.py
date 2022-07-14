52d7d1fb4ca6d607188d8819ad2c061aaa5ed532
prince482 <tejindersingh2202@gmail.com>
2022-07-15 02:26:13 +0530

    Grade

---
+++ loop/graduate.py
@@ -0,0 +1,15 @@
+''' 
+Grade finding program
+'''
+percentage = int(input('Enter the percentage of the student='))
+if percentage>=90:
+    print('The grade of the student is A')
+elif percentage>=80:
+    print('The grade of the student is B')
+elif percentage>=70:
+    print('The grade of the student is C')
+elif percentage>=60:
+    print('The grade of the student is D')
+else:
+    print('The grade of the student is F')
+	

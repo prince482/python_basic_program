a56da5632d0a358a7af977b717986ae8e795389f
prince482 <tejindersingh2202@gmail.com>
2022-07-15 01:02:34 +0530

    positive negative

---
+++ loop/positive_negative.py
@@ -0,0 +1,8 @@
+'''
+finding the inputted number is positive or negative
+'''
+Number = int(input("Enter the number: -"))
+if Number < 0:
+ print(Number," is negative")
+else:
+ print(Number," is positive")

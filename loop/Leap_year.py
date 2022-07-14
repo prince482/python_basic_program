40b74870de83317c35fd2e4beea50e50cdebacb2
prince482 <tejindersingh2202@gmail.com>
2022-07-15 02:39:40 +0530

    leap year

--- loop/Leap_year.py
+++ loop/Leap_year.py
@@ -4,7 +4,7 @@ program to find leap year or not
 Year = int(input("Enter the Year"))
 if((Year % 100 != 0) and  
      (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
+    print("Given Year is a leap Year")
   # Else it is not a leap year  
 else:  
     print ("Given Year is not a leap Year")  

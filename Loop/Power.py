91ebd0911f8d9375fe17553e4a02c1cc001700cd
prince482 <tejindersingh2202@gmail.com>
2022-07-17 00:02:41 +0530

    power of number

---
+++ Loop/Power.py
@@ -0,0 +1,8 @@
+'''
+find the power of two number'''
+number_1 = int(input("Enter the number"))
+number_2 = int(input("Enter the number"))
+power = number_1**number_2
+power_2 = number_2**number_1
+print(f"THE POWER OF {number_1} is  {power}")
+print(f"THE POWER OF {number_2} is  {power_2}")
